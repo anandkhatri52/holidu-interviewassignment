688fd3f2d8d8270d8d7ff06a2c99fdfc5be5e12e
anandkhatri52 <31233835+anandkhatri52@users.noreply.github.com>
2019-06-15 16:20:38 +0530

    Update controller to get request param.

--- src/main/java/com/holidu/interview/assignment/support/HealthCheckController.java
+++ src/main/java/com/holidu/interview/assignment/support/HealthCheckController.java
@@ -1,11 +1,19 @@
 package com.holidu.interview.assignment.support;
 
+import com.holidu.interview.assignment.service.StreetTreeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HealthCheckController {
+
+    @Autowired
+    private StreetTreeService streetTreeService;
 
     @RequestMapping(
             name = "healthCheckEndpoint",
@@ -16,4 +24,15 @@ public class HealthCheckController {
         return "Greetings from the Holidu interview assignment!";
     }
 
+    @RequestMapping(
+            name = "retrieveCommonTreeEndpoint",
+            method = RequestMethod.GET,
+            value = "/tree"
+    )
+    public Map<String, Integer> retrieveCommonTree(
+            @RequestParam("latitude") double latitude,
+            @RequestParam("longitude") double longitude,
+            @RequestParam("radius") double radius) {
+        return streetTreeService.retrieveCommonStreetTree(latitude, longitude, radius);
+    }
 }
